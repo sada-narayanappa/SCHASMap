3222f5c86e0b061c47ccb9478353851782dc8e6a
Sada <sada.narayanappa@gmail.com>
2014-11-03 12:47:34 -0700

    sada

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 echo "***** EXECUTING GIT PULL ON OUR RESEARCH SERVER ***** students"
-echo "***"
+echo "****"
 
 ssh www.geospaces.org   "cd /var/www/html/SCHASMap; git pull"  
 # scp target/*.war   www.geospaces.org:/usr/local/tomcat/webapps
