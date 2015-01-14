f6f8b102c12c2878996ffa86c03dd48f7c141c43
Wade Meyers <meyersw3476@my.uwstout.edu>
2015-01-13 18:38:23 -0600

    "Find My Location" Functionality

--- html/maps/os.js
+++ html/maps/os.js
@@ -2,5 +2,17 @@
 //
 
 function findLocation() {
-   alert("Find Location and set map to center")
+   if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+	var foundPosition = new OpenLayers.LonLat(position.coords.longitude, position.coords.latitude).transform(
+              new OpenLayers.Projection("EPSG:4326"),
+              new OpenLayers.Projection("EPSG:900913")
+      );
+	map.setCenter(foundPosition, 14);	
 }
