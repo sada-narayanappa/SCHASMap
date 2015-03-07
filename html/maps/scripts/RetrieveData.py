debdedd2e514227f33fb43109d69d3fa78043c72
Wade Meyers <meyersw3476@my.uwstout.edu>
2015-03-07 11:06:29 -0600

    Updated the scripts to match the column changes of the DB. Will need to come up with a better way of handling this to prevent it from happening again.

--- html/maps/scripts/RetrieveData.py
+++ html/maps/scripts/RetrieveData.py
@@ -2,6 +2,7 @@
 import urllib, urllib2, json
 
 def requestData():
+    
     #Requesting null weather data and putting it into JSON
     geourl = 'http://www.geospaces.org/aura/webroot/db.jsp?'
     geoVals = { 'qn' : '10'
@@ -14,8 +15,8 @@ def requestData():
     #Formatting for JSON
     the_page = the_page[35:] #Remove js information 
     the_page = the_page[:1] + '"'+the_page[1:5]+'"' +the_page[5:]
-    the_page = the_page[:-739]+'"' + the_page[-739:-731]+ '"' + the_page[-731:-341] + '"' + the_page[-341:-333]+'"'+the_page[-333:]
-    the_page = the_page[:-748]+the_page[-747:]
+    the_page = the_page[:-760]+'"' + the_page[-760:-752]+ '"' + the_page[-752:-351] + '"' + the_page[-351:-343]+'"'+the_page[-343:]
+    the_page = the_page[:-769]+the_page[-768:]
     
     jsonData = json.loads(the_page)
     
