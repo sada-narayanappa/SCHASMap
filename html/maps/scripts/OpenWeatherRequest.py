dd98bf13bc81159dd02f05a355780b14409b6726
Wade Meyers <meyersw3476@my.uwstout.edu>
2015-03-31 15:20:25 -0500

    Will not attempt historical update if OpenWeatherMap History server is down. Also retrieves cloud % now.

--- html/maps/scripts/OpenWeatherRequest.py
+++ html/maps/scripts/OpenWeatherRequest.py
@@ -11,7 +11,7 @@ def OpenWeatherRequest(jsonData):
     k=0
     OWeatherData = []
     # len(jsonData["rows"])
-    while k < 10: #Limited to 10 at a time due to timeout error's with larger amounts.
+    while k < 1: #Limited to 1 at a time due to timeout error's with larger amounts.
         OWeatherData.append([]) #new list for the current entry
             
         entryID = jsonData["rows"][k][0]
@@ -20,82 +20,147 @@ def OpenWeatherRequest(jsonData):
         lon = jsonData["rows"][k][12]
         
         
-        #FINDING THE NEAREST STATION TO THE GEO COORD
-        findStationURL = 'http://api.openweathermap.org/data/2.5/station/find?'
-        latLonValues ={'lat' : str(lat),
-                'lon' : str(lon),
-                'cnt' : '1',
-                'APPID' : '31dbfeee1aca334615ad89b93bf440b6' }    
-        data = urllib.urlencode(latLonValues)
-        req = urllib2.Request(findStationURL + data)
-        response = urllib2.urlopen(req)
-        the_page = response.read()
-        jsonDataStation = json.loads(the_page)[0]
-        stationID = jsonDataStation['station']['id'] #THE nearest station id
-        
-        #using the station ID to find the weather for the correct time
-        url = 'http://api.openweathermap.org/data/2.5/history/station?'
-        values = { 'APPID' : '31dbfeee1aca334615ad89b93bf440b6',
-                'cnt' : '1',
-                'type' : 'hour',
-                'start' : str(entryUnix-43200),
-                'end' : str(entryUnix+43200),
-                'id' : str(stationID)}
-        data = urllib.urlencode(values) #DATA SHOULD GO AT THE END OF THE URL
-        req = urllib2.Request(url+data)
-        response = urllib2.urlopen(req)
-        
-        the_page = response.read()
-        
-        jsonDataOWeather = json.loads(the_page)
-        
-        
-        #Finding the closest time in the server response
-        i = 0
-        weather_time = 0
-        closestTimeID = 0
-        timeDiff = 999999999
-        while i < len(jsonDataOWeather['list']):
-            tempTimeDiff = abs(int(jsonDataOWeather['list'][i]['dt'])-entryUnix)
+        if((time.time()-entryUnix) > 3600):
+            #FINDING THE NEAREST STATION TO THE GEO COORD
+            findStationURL = 'http://api.openweathermap.org/data/2.5/station/find?'
+            latLonValues ={'lat' : str(lat),
+                    'lon' : str(lon),
+                    'cnt' : '1',
+                    'APPID' : '31dbfeee1aca334615ad89b93bf440b6' }    
+            data = urllib.urlencode(latLonValues)
+            req = urllib2.Request(findStationURL + data)
+            response = urllib2.urlopen(req)
+            the_page = response.read()
+            jsonDataStation = json.loads(the_page)[0]
+            stationID = jsonDataStation['station']['id'] #THE nearest station id
             
-            if tempTimeDiff < timeDiff:
-                weather_time = int(jsonDataOWeather['list'][i]['dt'])
-                timeDiff = tempTimeDiff
-                closestTimeID = i
+            #using the station ID to find the weather for the correct time
+            url = 'http://api.openweathermap.org/data/2.5/history/station?'
+            values = { 'APPID' : '31dbfeee1aca334615ad89b93bf440b6',
+                    'cnt' : '1',
+                    'type' : 'hour',
+                    'start' : str(entryUnix-43200),
+                    'end' : str(entryUnix+43200),
+                    'id' : str(stationID)}
+            data = urllib.urlencode(values) #DATA SHOULD GO AT THE END OF THE URL
+            req = urllib2.Request(url+data)
             
-            i=i+1
-        
-        
-        #Extract the data from the closest match
-        
-        try:
-            temperatureMinKelvin = jsonDataOWeather['list'][closestTimeID]['temp']['mi']
-        except:
-            temperatureMinKelvin = None
-        
-        try:
-            temperatureMaxKelvin = jsonDataOWeather['list'][closestTimeID]['temp']['ma']
-        except:
-            temperatureMaxKelvin = None
+            serverUp = True
+            try:
+                response = urllib2.urlopen(req)
+            except:
+                serverUp = False
+                temperatureMinKelvin = None
+                temperatureMaxKelvin = None
+                humidity = None
+                pressure = None
+                wind = 'null'
+                clouds = 'null'
             
-        try:
-            humidity = jsonDataOWeather['list'][closestTimeID]['humidity']['v']
-        except:
-            humidity = None
+            if serverUp:
+                the_page = response.read()
+                
+                jsonDataOWeather = json.loads(the_page)
+                
+                
+                #Finding the closest time in the server response
+                i = 0
+                weather_time = 0
+                closestTimeID = 0
+                timeDiff = 999999999
+                while i < len(jsonDataOWeather['list']):
+                    tempTimeDiff = abs(int(jsonDataOWeather['list'][i]['dt'])-entryUnix)
+                    
+                    if tempTimeDiff < timeDiff:
+                        weather_time = int(jsonDataOWeather['list'][i]['dt'])
+                        timeDiff = tempTimeDiff
+                        closestTimeID = i
+                    
+                    i=i+1    
+            
+                #Extract the data from the closest match
+                
+                try:
+                    temperatureMinKelvin = jsonDataOWeather['list'][closestTimeID]['temp']['mi']
+                except:
+                    temperatureMinKelvin = None
+                
+                try:
+                    temperatureMaxKelvin = jsonDataOWeather['list'][closestTimeID]['temp']['ma']
+                except:
+                    temperatureMaxKelvin = None
+                    
+                try:
+                    humidity = jsonDataOWeather['list'][closestTimeID]['humidity']['v']
+                except:
+                    humidity = None
+                
+                try:
+                    pressure = jsonDataOWeather['list'][closestTimeID]['pressure']['v']
+                except:
+                    pressure = None
+                
+                try:
+                    wind = jsonDataOWeather['list'][closestTimeID]['wind']['v']
+                except:
+                    wind = 'null'
+                try:
+                    clouds = jsonDataOWeather['list'][closestTimeID]['clouds']['all']
+                except:
+                    clouds = 'null'
+                
         
-        try:
-            pressure = jsonDataOWeather['list'][closestTimeID]['pressure']['v']
-        except:
-            pressure = None
-        
-        try:
-            wind = jsonDataOWeather['list'][closestTimeID]['wind']['v']
-        except:
-            wind = 'null'
-        
+        if((time.time()-entryUnix) <= 3600):
+            url = 'http://api.openweathermap.org/data/2.5/weather?'
+            values = { 'APPID' : '31dbfeee1aca334615ad89b93bf440b6',                
+                    'lat' : str(lat),
+                    'lon' : str(lon)
+                    }
+            data = urllib.urlencode(values) #DATA SHOULD GO AT THE END OF THE URL
+            req = urllib2.Request(url+data)
+            response = urllib2.urlopen(req)
+            
+            the_page = response.read()
+            
+            jsonDataOWeather = json.loads(the_page)
+            
+            try:
+                temperatureMinKelvin = jsonDataOWeather['main']['temp_min']
+            except:
+                temperatureMinKelvin = None
+            
+            try:
+                temperatureMaxKelvin = jsonDataOWeather['main']['temp_max']
+            except:
+                temperatureMaxKelvin = None
+                
+            try:
+                humidity = jsonDataOWeather['main']['humidity']
+            except:
+                humidity = None
+            
+            try:
+                pressure = jsonDataOWeather['main']['pressure']
+            except:
+                pressure = None
+            
+            try:
+                wind = jsonDataOWeather['wind']['speed']
+            except:
+                wind = 'null'
+            try:
+                clouds = jsonDataOWeather['clouds']['all']
+            except:
+                clouds = 'null'
+            try:
+                weather_time = jsonDataOWeather['dt']
+            except:
+                weather_time = 'null'
+            
         
         #convert weather_time from unix to standard time
-        weather_time = datetime.datetime.fromtimestamp(weather_time).strftime('%Y-%m-%d %H:%M:%S')
+        if weather_time != 'null':
+            weather_time = datetime.datetime.fromtimestamp(weather_time).strftime('%Y-%m-%d %H:%M:%S')
         
         OWeatherData[k].append(weather_time)
         OWeatherData[k].append(temperatureMinKelvin)
@@ -104,6 +169,7 @@ def OpenWeatherRequest(jsonData):
         OWeatherData[k].append(pressure)
         OWeatherData[k].append(wind)
         OWeatherData[k].append(entryID)
+        #OWeatherData[k].append(clouds)
         
         k=k+1
     
