86486849c07479f796943714fafaf85011c57318
Sada <sada.narayanappa@gmail.com>
2015-04-20 11:57:34 -0600

    Commiting by Sada

---
+++ html/js/useroptions.js
@@ -0,0 +1,14 @@
+var userOptions = {
+   myMobileID:    "",
+   myMapView:     [0,0,0,0],
+   myLayers:      ["layer1", "layer2"]
+
+}
+
+function getUserOptions() {
+
+}
+
+function saveUserOptions() {
+
+}
