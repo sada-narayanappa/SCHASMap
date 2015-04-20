d9eb3559a5d6e315e8b670622601eee717b85601
Sada <sada.narayanappa@gmail.com>
2015-04-20 11:59:58 -0600

    Commiting by Sada

--- html/js/useroptions.js
+++ html/js/useroptions.js
@@ -1,14 +1,16 @@
 var userOptions = {
    myMobileID:    "",
    myMapView:     [0,0,0,0],
-   myLayers:      ["layer1", "layer2"]
-
+   myLayers:      ["layer1", "layer2"],
+   myUserID:      ""
 }
 
+// Read the userOptions JSON from cookie named USER_OPTIONS
 function getUserOptions() {
 
 }
 
+// SAVE  userOptions JSON from cookie named USER_OPTIONS
 function saveUserOptions() {
 
 }
