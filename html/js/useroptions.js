0b6a9c2cc32fab53c96fbee98959857746fff730
jihadaj <jihadaj@yahoo.com>
2015-04-29 11:58:51 +0400

    edit the openlayer3.html for saving the userSession

--- html/js/useroptions.js
+++ html/js/useroptions.js
@@ -35,7 +35,7 @@ function getUserOptions() {
 }
 // SAVE  userOptions JSON from cookie named USER_OPTIONS
 function setUserOptions(map) {
-   console.log("called setUserOptions");
+  // console.log("called setUserOptions");
    //map.zoom = userOptions.zoom;
    var cp = cPoint1(userOptions.centerLon, userOptions.centerLat)
    var lonlat = new OpenLayers.LonLat(cp.x, cp.y);
@@ -45,20 +45,29 @@ function setUserOptions(map) {
 
 }
 function setMapLayers(map) {
+   //printAllMapLayers(map);
+   for ( j=0;  j < map.layers.length; j++) {
+      map.layers[j].setVisibility(false);
+   }
 
    for( i= 0;i<userOptions.myLayers.length;i++) {
-     // console.log("layers available in  useroptions are "+userOptions.myLayers[i]);
-      for ( j = 0; j < map.layers.length; j++) {
-         if (map.layers[j].name == userOptions.myLayers[i]) {
-            map.layers[j].visibility = true;
+   for ( j=0;  j < map.layers.length; j++) {
+         if (userOptions.myLayers[i]==map.layers[j].name) {
+            map.layers[j].setVisibility(true);
             //console.log("visible map layer"+map.layers[j].name);
+            break;
          }
       }
    }
+   //printVisibleMapLayers(map);
 }
 
 // SAVE  userOptions JSON from cookie named USER_OPTIONS
 function saveUserOptions(map) {
+   console.log("called saveUserOptions");
+   /*printAllMapLayers(map);
+   printVisibleMapLayers(map);
+   printInVisibleMapLayers(map);*/
    updateMapLayers(map)
    p = cPoint(map.center)
    userOptions.centerLon = p.x;
@@ -76,9 +85,46 @@ function updateMapLayers(map) {
    for( i= 0, j=0;i<map.layers.length;i++) {
       if(map.layers[i].visibility){
          userOptions.myLayers[j]=map.layers[i].name;
-         console.log("layers saved to useroptions are "+userOptions.myLayers[j]);
+         //console.log("layers saved to useroptions are "+userOptions.myLayers[j]);
          j++;
       }
    }
-
+   //printUserOptonsLayers();
 }
+
+/* HELPER FUNCTIONS FOR DEBUGGING */
+
+function printVisibleMapLayers(map) {
+   console.log("******************* Visible Visible Map layers are :******************")
+   for (i = 0; i < map.layers.length; i++) {
+      if (map.layers[i].visibility) {
+         console.log(  map.layers[i].name)
+      }
+   }
+   console.log("******************* End of visible Map layers *****************")
+}
+function printInVisibleMapLayers(map) {
+   console.log("-----------------Invisible  Map layers are :--------------");
+   for (i = 0; i < map.layers.length; i++) {
+      if (!map.layers[i].visibility) {
+         console.log(  map.layers[i].name)
+      }
+   }
+   console.log("----------------End of Invisible Map layers----------------------");
+}
+
+function printAllMapLayers(map) {
+   console.log(" &&&&&&&&&&&&&&&&&&&&&&&&& All the map layers are &&&&&&&&&&&&&&&&&&&& :")
+   for (i = 0; i < map.layers.length; i++) {
+         console.log(  map.layers[i].name)
+   }
+   console.log("&&&&&&&&&&&&&&&&&&&&&&&&& end of all  Map layers are &&&&&&&&&&&&&&&&&&&&");
+}
+function printUserOptonsLayers() {
+   console.log(" !!!!!!!!!!!!!!!!!!!!!!!! User selected layers are !!!!!!!!!!!!!!!!!!!!!!!! :");
+   for( i= 0;i<userOptions.myLayers.length;i++) {
+      console.log(userOptions.myLayers[i]);
+      }
+   console.log("!!!!!!!!!!!!!!!!!!!  end of user selected layers !!!!!!!!!!!!!!!!!!!!!!!!");
+   }
+
