c8b966e7fd9ba8b962fe0fe27eb5b3c3657b1651
Wade Meyers <meyersw3476@my.uwstout.edu>
2015-06-06 14:06:22 -0500

    Added more console logs to figure out why it is not recognizing the middle segments of routes.

--- html/maps/possibleRoutesLayer.js
+++ html/maps/possibleRoutesLayer.js
@@ -283,9 +283,11 @@ possibleRoutesLayer.prototype.AddFeatures = function (data, zoomToBounds){
       
       var middle = false;
       //check to see if current entry is the middle of a route (to display total miles popup)
+      console.log("Found middle:" + i);
+      console.log(Math.floor(edgesInRoute[lc[1]]/2));
       if(i === Math.floor(edgesInRoute[lc[1]]/2)){
           middle = true; 
-          console.log("Found middle:" + i)
+          console.log("Found middle:" + i);
       }
       
       $rss = JSON.parse(lc[5]);
