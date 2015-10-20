d26e0cf7a2aeef648b8eabcd82f56b35b1b66ea9
Wade Meyers <meyersw3476@my.uwstout.edu>
2015-10-20 14:29:12 -0600

    Had to create a new proxy directory path for the data page.

--- html/js/config.prod.js
+++ html/js/config.prod.js
@@ -5,7 +5,8 @@ var config =
    hosts:      "www.geospaces.org;10.0.0.3",
    hostsArray: [],
    host :      "www.geospaces.org",
-   PROXY:      "../cgi-bin/proxy.py?url="
+   PROXY:      "../cgi-bin/proxy.py?url=",
+   PROXY2:      "../../cgi-bin/proxy.py?url="
 
 }
 
